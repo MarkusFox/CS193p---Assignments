8949c8f9112cb2ff4aa6fcfb9412852a0a7604ed
MarkusFox <markus.fox90@gmail.com>
2021-12-30 17:33:12 +0100

    extra credit - assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -15,15 +15,15 @@ struct ContentView: View {
     ]
     
     @State var selectedTheme: String = "vehicles"
-//    @State var emojiCount: Int = 10
+    @State var emojiCount: Int = 10
     
     var body: some View {
         VStack {
             Text("Memorize!").font(.title)
             Spacer()
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))]) {
-                    ForEach(emojis[selectedTheme]!.shuffled(), id: \.self, content: { emoji in
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: widthThatBestFits(cardCount: emojiCount)))]) {
+                    ForEach(emojis[selectedTheme]!.shuffled()[0..<emojiCount], id: \.self, content: { emoji in
                         CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
                     })
                 }.foregroundColor(.purple) // default will get passed to all inside Stack
@@ -42,9 +42,20 @@ struct ContentView: View {
             .padding(.all)
     }
     
+    func widthThatBestFits(cardCount: Int) -> CGFloat {
+        if cardCount <= 9 {
+            return 80.0
+        } else if cardCount > 16 {
+            return 50.0
+        } else {
+            return 60.0
+        }
+    }
+    
     var vehicles: some View {
         Button(action: {
             selectedTheme = "vehicles"
+            emojiCount = Int.random(in: 4...emojis[selectedTheme]!.count-1)
         }, label: {
             VStack {
                 Image(systemName: "car")
@@ -56,6 +67,7 @@ struct ContentView: View {
     var animals: some View {
         Button(action: {
             selectedTheme = "animals"
+            emojiCount = Int.random(in: 4...emojis[selectedTheme]!.count-1)
         }, label: {
             VStack {
                 Image(systemName: "hare")
@@ -67,6 +79,7 @@ struct ContentView: View {
     var hearts: some View {
         Button(action: {
             selectedTheme = "hearts"
+            emojiCount = Int.random(in: 4...emojis[selectedTheme]!.count-1)
         }, label: {
             VStack {
                 Image(systemName: "heart")
